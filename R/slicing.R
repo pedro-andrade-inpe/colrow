d5d92174f2efbc6e9e47fd48e48327ff8eaab077
Pedro R. Andrade <pedro.andrade@inpe.br>
2018-06-11 12:28:38 -0300

    Close #2. New function equalSteps.

---
+++ R/slicing.R
@@ -0,0 +1,22 @@
+
+#' @title Creates intervals with equal sizes to be used by plot functions
+#' @description Creates intervals with equal sizes from min/max values to be
+#' used by plot functions. If data is provided, it gets min/max from data.
+#' @param min The minimum value
+#' @param max The maximum value
+#' @param slices The number of slices to be created. Note that, when you have
+#' n slices, the legend will have n - 1 colors.
+#' @param data Any data that can be converted to a data.frame. The first column
+#' of the data will be used to compute min and max.
+#' @usage
+#' equalslices(0, 100, 5)
+#' @export
+equalSteps <- function(min = NULL, max = NULL, slices, data = NULL){
+  if(!is.null(data)){
+    values = as.data.frame(data)[,1]
+    max <- max(values)
+    min <- min(values)
+  }
+
+  rep(min, slices) + (max - min) / (slices - 1) * 0:(slices - 1)
+}
---
+++ demo/spplot.R
@@ -0,0 +1,42 @@
+
+require(rgdal)
+require(RColorBrewer)
+require(simu)
+
+brazil_cr <- system.file("results", "IDC_Amazon.shp", package = "simu") %>% readOGR()
+biomes    <- system.file("extdata/shape", "biomas_br_pol.shp", package = "simu") %>% readOGR()
+states    <- system.file("extdata/shape", "estados_br_pol.shp", package = "simu") %>% readOGR()
+amzlegal  <- system.file("extdata/shape", "amazlegal.shp", package = "simu") %>% readOGR()
+
+statessp   <- list("sp.polygons", states,   fill = "transparent", col = "black", add = TRUE)
+amzlegalsp <- list("sp.polygons", amzlegal, fill = "transparent", col = "black", add = TRUE, lty = 2, first = FALSE)
+biomessp   <- list("sp.polygons", biomes,   fill = "transparent", col = "black", add = TRUE, first = FALSE)
+
+oranges      <- brewer.pal(9, "Oranges")
+blues        <- brewer.pal(9, "Blues")
+greenOranges <- c("green",brewer.pal(8, "Oranges"))
+reds         <- brewer.pal(9, "Reds")
+greys        <- brewer.pal(9, "Greys")
+ylGn         <- brewer.pal(9, "YlGn")
+rdPu         <- brewer.pal(9, "RdPu")
+
+cuts <-c (0.0, 5.92, 16.76, 37.33, 71.11, 130.38, 211.54, 260, 294, 308)
+
+spplot(
+  brazil_cr[,"Rice2000"],
+  at = cuts,
+  col = "transparent", # remove this line to draw the border of each CR
+  col.regions = oranges,
+  sp.layout = list(biomessp, amzlegalsp)
+)
+
+cuts <- equalSteps(data = brazil_cr[,"Rice2000"], steps = 6)
+cuts
+
+spplot(
+  brazil_cr[,"Rice2000"],
+  at = cuts,
+  col = "transparent", # remove this line to draw the border of each CR
+  col.regions = oranges,
+  sp.layout = list(biomessp, amzlegalsp)
+)
