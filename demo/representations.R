42190d0769f79ec506290a4552a01960b199b6f1
pedro-andrade-inpe <pedro.andrade@inpe.br>
2018-09-20 12:54:48 -0300

    Updating representations example to save three shapefiles.

--- demo/representations.R
+++ demo/representations.R
@@ -1,6 +1,4 @@
 
-require(colrow)
-
 # First download data from:
 # https://www.dropbox.com/sh/sqocqe45jwmug2p/AAAbv-IAg24a_R4vYsP9zqV_a?dl=0
 
@@ -20,7 +18,7 @@ plot(sf::st_geometry(mySimU), main = "SimU", col = "red"); box()
 plot(sf::st_geometry(myCR),   main = "CR", col = "blue");   box()
 plot(sf::st_geometry(myLU),   main = "LU", col ="green");   box()
 
-#sf::write_sf(myLU, "colombiaLU.shp")
+sf::write_sf(myLU, paste0(country, "LU.shp"))
+sf::write_sf(myCR, paste0(country, "CR.shp"))
+sf::write_sf(mySimU, paste0(country, "SimU.shp"))
 
-#argLU <- getLU("argentina", data) # error. see the message and fix the command
-#brazilLU <- getLU("Brazil", data)
